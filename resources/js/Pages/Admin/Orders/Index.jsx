209d827f328484bbaaae8f0d60903a4516991c8b
kevinmakwana <kevinmakwana189@gmail.com>
2025-12-27 22:28:27 +0530

    remove unused code

--- resources/js/Pages/Admin/Orders/Index.jsx
+++ resources/js/Pages/Admin/Orders/Index.jsx
@@ -58,24 +58,6 @@ export default function Index({ orders, filters, statuses, can }) {
         });
     };
 
-    const handleDelete = (id) => {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                router.delete(route('admin.orders.destroy', id), {
-                    preserveScroll: true,
-                });
-            }
-        });
-    };
-
     const getStatusBadgeClass = (status) => {
         const statusClasses = {
             'pending': 'bg-yellow-100 text-yellow-800',
